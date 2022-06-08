feb164801dc16962c10d83182d013e60c8f3763a
Rick Scavetta <office@scavetta.academy>
2022-06-08 16:10:08 +0200

    add pairs plot

--- 02-r/02-1-EDA-examples.R
+++ 02-r/02-1-EDA-examples.R
@@ -3,6 +3,7 @@
 # Initialize packages
 library(ggplot2)
 library(DataExplorer)
+library(GGally)
 
 # Decide on an easy dataset 
 PlantGrowth
@@ -14,3 +15,7 @@ create_report(diamonds,
               output_file = "diamonds_EDA.html", 
               output_dir = "02-r/EDA_reports/")
 
+# GGally pairs plot
+g <- ggpairs(diamonds)
+ggsave(g, filename = "02-r/EDA_reports/diamonds_pairs.png", width = 10, height = 10, units = "in")
+ggsave(g, filename = "02-r/EDA_reports/diamonds_pairs_largeview.png", width = 20, height = 20, units = "in")
