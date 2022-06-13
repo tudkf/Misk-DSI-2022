526239003ad2094683a5f1d3df7917eab392cec8
Rick Scavetta <office@scavetta.academy>
2022-06-13 15:32:03 +0200

    update R indexing

--- 02-r/04-Indexing.R
+++ 02-r/04-Indexing.R
@@ -9,13 +9,20 @@
 foo1
 # Try to find the following:
 # The 6th element
+foo1[6]
 # The pth element
+foo1[p]
 # The 3rd to the pth element
+foo1[3:p]
 # the pth to the last element
+foo1[p:length(foo1)]
 
 # Remove using - (e.g. 11 - 15th element)
 foo1[-11:-15]
 foo1[-(11:length(foo1))]
+
+-(6:8)
+-1 * 6:8
 
 # Remove using - (e.g. 11 & 15th element)
 foo1[c(-11,-15)]
@@ -58,7 +65,7 @@ foo1 < 45 # produces a logical vector,
 foo1[foo1 < 45] 
 
 # Lists (1-dimensional, hetero)
-# Use [] or [[]]for each element 
+# Use [] or [[]] for each element 
 # Each of the 13 elements has a name, 
 names(plant_lm)
 str(plant_lm)
@@ -100,6 +107,9 @@ foo_df <- as.data.frame(foo_df)
 class(foo_df)
 foo_df
 # repeat the solutions from above:
+foo_df[,3] # as data.frame
+foo_df[3] # as a data.frame
+
 
 # ===========================================
 # Exercise ----
@@ -116,7 +126,7 @@ foo_df <- as_tibble(foo_df)
 
 # We can also use names:
 foo_df[,"healthy"]
-
+names(foo_df)
 # But don't forget logical vectors:
 # e.g. which tissues have low quantity (below 10)?
 foo_df[foo_df$quantity < 10, "tissue"]
@@ -132,6 +142,40 @@ foo_df$quantity[foo_df$tissue == "Heart"]
 foo_df %>% 
   filter(quantity < 10) %>% 
   select(tissue)
+
+foo_df %>% 
+  slice(2:4) %>% 
+  select(tissue)
+
+# PlantGrowth
+# What is the 3rd highest weight in
+# each of the 3 groups of the group column
+yy <- c(6, 1, 3)
+sort(yy) # short for yy[order(yy)]
+rank(yy)
+order(yy) # index position of the original vector
+yy[order(yy)]
+
+PlantGrowth %>% 
+  group_by(group) %>% 
+  arrange(group, -weight) # order the data frame according to weight
+
+PlantGrowth %>% 
+  group_by(group) %>% 
+  arrange(group, desc(weight)) %>% 
+  slice(1:3)
+
+PlantGrowth %>% 
+  group_by(group) %>% 
+  arrange(group, desc(weight)) %>% 
+  summarise(weight = weight[1:3])
+
+# head(.,3)
+# PlantGrowth %>%
+#   .[3,]
+
+# foo_df %>% 
+#   [quantity < 10, tissue]
 
 # Using integer vectors instead of logical vectors
 # base:
